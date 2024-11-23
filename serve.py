ef246670ec3a5f5ab318c2565c2bb12e698ded59
Srustiashline <srustiashline6@gmail.com>
2024-11-23 13:17:52 +0530

    Update serve.py

--- serve.py
+++ serve.py
@@ -18,5 +18,9 @@ def entry():
 	return res
 
 
+import os
+
 if __name__ == "__main__":
-	app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use PORT environment variable or default to 5000
+    app.run(host="0.0.0.0", port=port)
+
